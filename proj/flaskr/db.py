321d058a2f7a76e639e70ab50746978f44d01138
Tyler Gelinas <webshockinnovations@gmail.com>
2019-10-07 21:55:46 -0400

    Change over from SQLlite to tethys databse.  Use PyMySQL for easier queries

--- proj/flaskr/db.py
+++ proj/flaskr/db.py
@@ -1,33 +1,39 @@
-import sqlite3
 
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+import pymysql
+import flaskr.config
+class Database:
+    def __init__(self) :
+        host = flaskr.config.host
+        user = flaskr.config.user
+        password = flaskr.config.password
+        db = flaskr.config.db
+        self.con = pymysql.connect(host=host, user=user, password=password, database=db, cursorclass=pymysql.cursors.
+                                   DictCursor)
+        self.cur = self.con.cursor()
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
+    def insert(self, statement) :
+        return self.cur.execute(statement)
 
 
-def init_db():
-    db = get_db()
+    def select(self, statement) :
+        self.cur.execute(statement)
+        result = self.cur.fetchone()
+        return result
 
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+    def close_db(self, e=None) :
+        db = g.pop('db', None)
+        if db is not None:
+            db.close()
+
+
+    def init_db(self):
+        db = get_db()
+        with current_app.open_resource('schema.sql') as f:
+            db.executescript(f.read().decode('utf8'))
 
 
 @click.command('init-db')
@@ -39,5 +45,5 @@ def init_db_command():
 
 
 def init_app(app):
-    app.teardown_appcontext(close_db)
+    app.teardown_appcontext(Database.close_db)
     app.cli.add_command(init_db_command)
