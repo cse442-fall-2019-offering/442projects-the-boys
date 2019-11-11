102916ca2ce17f617a503daa85fc58e5ef078d7c
Dannen <dantheman5398@gmail.com>
2019-11-10 23:40:33 -0500

    Leaderboards can filter friends

--- proj/flaskr/leaderboards.py
+++ proj/flaskr/leaderboards.py
@@ -13,10 +13,35 @@ def leaderboard():
 
     if request.method == 'POST':
         criteria = request.form['criteria']
-        peoples = db.selectall(
-            "SELECT * FROM user WHERE occupation IS NOT NULL"
-            " ORDER BY {}".format(criteria)
-        )
+        friendFilter = False
+        if 'friendFilter' in request.form:
+            friendFilter = True
+        print(friendFilter)
+        if friendFilter:
+            if criteria == "username":
+                criteria = "user_name"
+            elif criteria == "income":
+                criteria = "income"
+            elif criteria == "occupation":
+                criteria = "field"
+            elif criteria == "location":
+                criteria = "location"
+            print("SELECT * FROM friends WHERE friend_id = {}"
+                " ORDER BY {}".format(session.get('user_id'), criteria))
+            peoples = db.selectall(
+                "SELECT * FROM friends WHERE friend_id = {}"
+                " ORDER BY {}".format(session.get('user_id'), criteria)
+            )
+            print(peoples)
+            for person in peoples:
+                person["username"] = person["user_name"]
+                person["occupation"] = person["field"]
+
+        else:
+            peoples = db.selectall(
+                "SELECT * FROM user WHERE occupation IS NOT NULL"
+                " ORDER BY {}".format(criteria)
+            )
         return render_template('leaderboards/default_leader.html', people=peoples, cat=criteria)
 
     peoples = db.selectall(
