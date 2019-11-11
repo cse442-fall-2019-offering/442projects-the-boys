40ad5196014083f57c349b1d4f829f5fba449f1a
Tyler Gelinas <webshockinnovations@gmail.com>
2019-11-11 02:33:25 -0500

    Some settings page updates regarding updating usernames and full names

--- proj/flaskr/settings.py
+++ proj/flaskr/settings.py
@@ -1,13 +1,43 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, session
 )
 from werkzeug.exceptions import abort
-
+import flaskr.db
 bp = Blueprint('settings', __name__, url_prefix='/settings')
 
 
-@bp.route('/index')
+@bp.route('/index', methods=['GET', 'POST'])
 def settings():
     if g.user is None:
         return redirect(url_for("auth.login"))
-    return render_template('settings/index.html')
+    db = flaskr.db.Database()
+    user = db.selectall(
+
+        "SELECT * FROM user WHERE id = '{}'".format(session.get('user_id'))
+
+    )
+
+    if request.method == 'POST':
+        name1 = request.form['fullname']
+        username = request.form['username']
+        anonymous = 0
+        try:
+            request.form['anonymous']
+            anonymous = 1
+        except :
+            anonymous = 0
+
+            db.insert(
+                "UPDATE user SET name1 = '{}' WHERE id='{}' ".format(name1, session.get('user_id'))
+            )
+            db.insert(
+                "UPDATE user SET username = '{}' WHERE id='{}' ".format(username, session.get('user_id'))
+            )
+
+            db.insert(
+                "UPDATE user SET anonymous = '{}' WHERE id='{}' ".format(anonymous, session.get('user_id'))
+            )
+
+
+
+    return render_template('settings/index.html', user=user)
