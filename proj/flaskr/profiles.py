86656273c749692cfca3534ecaa001b5fbcec33e
Tyler Gelinas <webshockinnovations@gmail.com>
2019-11-13 00:32:32 -0500

    Add charts to stuff

--- proj/flaskr/profiles.py
+++ proj/flaskr/profiles.py
@@ -66,7 +66,7 @@ def profile():
 
 
 
-
+    total = total_expenses['total'] // 12
 
 
     if request.method == 'POST':
@@ -122,7 +122,7 @@ def profile():
         user[0]['income'] = 0
 
 
-    return render_template('profiles/profile.html', expenses=expenses,total_expenses=total_expenses, user=user[0],friends=friends,numFriends=numFriends)
+    return render_template('profiles/profile.html',infographics=infographics(total, user[0]['income'] // 12), expenses=expenses,total_expenses=total_expenses, user=user[0],friends=friends,numFriends=numFriends)
 
 @bp.route('/startAddFriends', methods=['POST','GET'])
 def startAddFriends():
@@ -145,12 +145,12 @@ def addFriends():
             )
             if id is None:
                 db.insert(
-                    "INSERT INTO friends VALUES ( '{}' , '{}' , '{}' , '{}' , '{}' , '{}' , '{}','{}' )".format(name['username'],
+                    "INSERT INTO friends VALUES ( '{}' , '{}' , '{}' , '{}' , '{}' , '{}' , '{}','{}' ,'{}')".format(name['username'],
                                                                                      session.get('user_id'),
                                                                                      name['occupation'],
                                                                                      name['location'],
                                                                                         name['name1'],
-                                                                                        name['age'], name['income'] , name['anonymous'])
+                                                                                        name['age'], name['income'] , name['anonymous'], name['id'])
 
                 )
                 return redirect(url_for('profiles.profile'))
@@ -159,7 +159,7 @@ def addFriends():
 
             if id['friend_id'] != session.get('user_id'):
                 db.insert(
-                "INSERT INTO friends VALUES ( '{}' , '{}' , '{}' , '{}' , '{}' , '{}', '{}','{}')".format(name['username'],session.get('user_id'),name['occupation'],name['location'],name['name1'], name['age'], name['income'],name['anonymous'])
+                "INSERT INTO friends VALUES ( '{}' , '{}' , '{}' , '{}' , '{}' , '{}', '{}','{}','{}')".format(name['username'],session.get('user_id'),name['occupation'],name['location'],name['name1'], name['age'], name['income'],name['anonymous'], name['id'])
 
                 )
             else:
@@ -174,12 +174,42 @@ def addFriends():
     return render_template('profiles/addFriends.html')
 
 
-@bp.route('/startViewFriend/<username>/<field>/<location>/<name>/<age>/<income>/<anon>',methods=['POST','GET'])
-def startViewFriend(username, field, location, name , age , income, anon):
+@bp.route('/startViewFriend/<username>/<field>/<location>/<name>/<age>/<income>/<anon>/<id>',methods=['POST','GET'])
+def startViewFriend(username, field, location, name , age , income, anon, id):
+    return redirect(url_for('profiles.viewFriend',username=username , field=field, location=location,name=name,age=age,income=income,anon=anon,id=id))
 
-    return redirect(url_for('profiles.viewFriend',username=username , field=field, location=location,name=name,age=age,income=income,anon=anon))
+@bp.route('/viewFriend/<username>/<field>/<location>/<name>/<age>/<income>/<anon>/<id>',methods=['POST','GET'])
+def viewFriend(username, field, location, name , age , income, anon, id):
+    db = flaskr.db.Database()
+    expenses = db.selectall(
+        "SELECT * FROM expense WHERE author_id = '{}'"
+        "ORDER BY category, cost".format(id)
+    )
 
-@bp.route('/viewFriend/<username>/<field>/<location>/<name>/<age>/<income>/<anon>',methods=['POST','GET'])
-def viewFriend(username, field, location, name , age , income, anon):
+    total_expenses = { "daily": 0, "weekly": 0, "monthly": 0, "yearly": 0, "oneTime": 0, "total": 0 }
 
-    return render_template('profiles/viewFriend.html', username=username , field=field, location=location,name=name,age=age,income=income,anon=anon )
+    for index, expense in enumerate(expenses) :
+        if expense['rate'] == 'Daily' :
+            total_expenses['daily'] = total_expenses['daily'] + expense['cost']
+        elif expense['rate'] == 'Weekly' :
+            total_expenses['weekly'] = total_expenses['weekly'] + expense['cost']
+        elif expense['rate'] == 'Monthly' :
+            total_expenses['monthly'] = total_expenses['monthly'] + expense['cost']
+        elif expense['rate'] == 'Yearly' :
+            total_expenses['yearly'] = total_expenses['yearly'] + expense['cost']
+        elif expense['rate'] == 'One Time' :
+            total_expenses['oneTime'] = total_expenses['oneTime'] + expense['cost']
+        expenses[index] = expense
+
+
+    total_expenses['total'] = total_expenses['daily'] * 365 + total_expenses['weekly'] * 52 + total_expenses['monthly'] * 12 + total_expenses['yearly'] + total_expenses['oneTime']
+    the_total_expenses = total_expenses['total'] // 12
+
+
+    return render_template('profiles/viewFriend.html', infographics=infographics(the_total_expenses, (int(income) // 12)), username=username , field=field, location=location,name=name,age=age,income=income,anon=anon,id=id )
+
+def infographics(spent, left) :
+    infographicdata = []
+    infographicdata.append(spent)
+    infographicdata.append(left - spent)
+    return infographicdata
